46e97f89f71eb47ea236d93acccb285462f63b5c
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-14 20:02:08 -0300

    created navar React component

--- src/components/React/Navar.tsx
+++ src/components/React/Navar.tsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
+import {
+  ClipboardList,
+  Plus,
+  Calendar,
+  CheckCircle,
+  Search,
+  Circle,
+  Menu,
+  X,
+} from "lucide-react";
 
 export const Navar = () => {
   const [menu, setMenu] = useState<boolean>(false);
   console.log(menu);
 
   return (
-    <nav>
+    <nav className="relative ">
       <button onClick={() => setMenu(!menu)} className="cursor-pointer">
-        Menu
+        {menu ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
       </button>
       {menu && (
-        <ul className="h-full flex flex-col gap-5">
+        <ul className=" h-dvh p-5 w-xs flex flex-col gap-5 absolute top-11 -right-5 bg-[#47FDB4]">
           <li>
             <a
               className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
               href="#"
             >
+              <Search className="w-5 h-5" />
               Buscar Tareas
             </a>
           </li>
@@ -24,6 +35,7 @@ export const Navar = () => {
               className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
               href="#"
             >
+              <Plus className="w-5 h-5" />
               Crear Tarea
             </a>
           </li>
@@ -32,6 +44,7 @@ export const Navar = () => {
               className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
               href="#"
             >
+              <Calendar className="w-5 h-5" />
               Calendario
             </a>
           </li>
@@ -40,7 +53,8 @@ export const Navar = () => {
               className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
               href="#"
             >
-              Tareas Incompletas
+              <CheckCircle className="w-5 h-5" />
+              Tareas Copletadas
             </a>
           </li>
           <li>
@@ -48,7 +62,8 @@ export const Navar = () => {
               className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
               href="#"
             >
-              Tareas Copletadas
+              <Circle className="w-5 h-5" />
+              Tareas Incompletas
             </a>
           </li>
         </ul>
