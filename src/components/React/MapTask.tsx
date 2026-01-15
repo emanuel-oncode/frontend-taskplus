3fb70aca0d952e50b86c721e35c97576197b56c9
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-15 16:31:46 -0300

    Terminando el componente que mapea las tareas

--- src/components/React/MapTask.tsx
+++ src/components/React/MapTask.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { type Task } from "../../types/Task.ts";
+import { CircleAlert, CircleCheck } from "lucide-react";
+import { Button } from "./Button.tsx";
 
 export const MapTask = () => {
   const [loading, setLoading] = useState(true);
@@ -17,10 +19,11 @@ export const MapTask = () => {
         if (!res.ok) {
           throw new Error("Error al obtener tareas");
         }
+        const result = await res.json();
 
-        const result: Task[] = await res.json();
+        const data: Task[] = result.tasks;
 
-        setTasks(result);
+        setTasks(data);
       } catch (error) {
         console.error("Error en fetch de tareas:", error);
         setLoading(false);
@@ -33,19 +36,32 @@ export const MapTask = () => {
   if (loading) return <div>Cargando tareas...</div>;
 
   return (
-    <article>
+    <article className="">
       {tasks.length > 0 ? (
         tasks.map((task) => (
-          <div key={task.task_id}>
-            <h3>{task.task_title}</h3>
+          <article
+            className="bg-gray-300 p-2 my-2 flex flex-col gap-2 items-start"
+            key={task.task_id}
+          >
+            <h3 className="text-xl font-bold">{task.task_title}</h3>
             <p>{task.task_description}</p>
             {task.task_completed ? (
-              <span>Tarea Completada</span>
+              <Button
+                task_id={task.task_id}
+                className="flex items-center gap-2"
+              >
+                <CircleCheck className="text-green-600" /> Tarea Completada
+              </Button>
             ) : (
-              <span>Tarea Incompleta</span>
+              <Button
+                task_id={task.task_id}
+                className="flex items-center gap-2"
+              >
+                <CircleAlert className="text-red-600" /> Tarea Incompleta
+              </Button>
             )}
             <span>{task.task_created_at.toString()}</span>
-          </div>
+          </article>
         ))
       ) : (
         <span>No hay tareas</span>
