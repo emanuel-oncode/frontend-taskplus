6d1ce8cf2ba32fe3dff6e4bfa02e1ec998e4c867
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-15 13:11:20 -0300

    navar para el footer

--- src/components/React/Navar.tsx
+++ src/components/React/Navar.tsx
@@ -1,75 +1,21 @@
 import { useState } from "react";
-import {
-  ClipboardList,
-  Plus,
-  Calendar,
-  CheckCircle,
-  Search,
-  Circle,
-  Menu,
-  X,
-} from "lucide-react";
+import { LayoutDashboard, ListTodo, Wallet } from "lucide-react";
 
 export const Navar = () => {
   const [menu, setMenu] = useState<boolean>(false);
   console.log(menu);
 
   return (
-    <nav className="relative">
-      <button onClick={() => setMenu(!menu)} className="cursor-pointer">
-        {menu ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
-      </button>
-      {menu && (
-        <ul className="h-125 p-5 w-xs flex flex-col gap-5 absolute top-11 -right-5 bg-[#ff0000]">
-          <li>
-            <a
-              className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
-              href="#"
-            >
-              <Search className="w-5 h-5" />
-              Buscar Tareas
-            </a>
-          </li>
-          <li>
-            <a
-              className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
-              href="#"
-            >
-              <Plus className="w-5 h-5" />
-              Crear Tarea
-            </a>
-          </li>
-          <li>
-            <a
-              className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
-              href="#"
-            >
-              <Calendar className="w-5 h-5" />
-              Calendario
-            </a>
-          </li>
-          <li>
-            <a
-              className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
-              href="#"
-            >
-              <CheckCircle className="w-5 h-5" />
-              Tareas Copletadas
-            </a>
-          </li>
-          <li>
-            <a
-              className="flex items-center gap-2 hover:text-[#3B5140] transition ease duration-200"
-              href="#"
-            >
-              <Circle className="w-5 h-5" />
-              Tareas Incompletas
-            </a>
-          </li>
-        </ul>
-      )}
-
-      <div></div>
+    <nav className="flex items-center justify-around w-full p-2">
+      <a href="#">
+        <LayoutDashboard />
+      </a>
+      <a href="#">
+        <ListTodo />
+      </a>
+      <a href="#">
+        <Wallet />
+      </a>
     </nav>
   );
 };
