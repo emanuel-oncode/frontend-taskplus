9b350898e99dcadc9fd00e8ab57acca0a41b138e
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-02-02 11:00:38 -0300

    añadiendo la pagina de login al sitio

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/Register" element={<RegisterPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
     </Routes>
   );
 }
